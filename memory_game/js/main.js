268a09df11510c4d34ff5957e5271ae35b51188c
Hung Muhamath <hungmuhamath@Jessis-MacBook-Pro.local>
2017-06-26 17:17:05 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,6 +21,7 @@ var cards = [
 		cardImage: "images/king-of-diamonds.png"
 	}
 ];
+var cardElement;
 var cardsInPlay = [];
 var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]){
@@ -29,13 +30,27 @@ var checkForMatch = function (){
 		alert("Sorry, try again.");
 	}
 };
-var flipCard = function (cardId){
 
+var flipCard = function (){
+	cardId = this.getAttribute('data-id');
+	this.setAttribute('src',cards[cardId].cardImage);
 	console.log ("User flipped " + cards[cardId].rank);
 	console.log (cards[cardId].cardImage);
 	console.log (cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank);	
+	cardsInPlay.push(cards[cardId].rank);
+	if (cardsInPlay.length === 2)
+	{checkForMatch();}	
 };
-flipCard (0);
-flipCard (2);
-checkForMatch();
+
+var createBoard = function (){
+	for (var i = 0; i < 4;i++){
+		cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id',i);
+
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+}
+createBoard();
+
